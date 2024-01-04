b35bda1e197c31630ba12e591ee10230e9597519
K-Nurakhmet <k_nura_95@mail.ru>
2024-01-04 13:43:22 +0500

    select refresh

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 
 const productBuy = prompt('Что вы хотите купить для ребёнка?');
 const goodsCount = 1;
-const productsCategory = 'Игровой приставки, игрушки, мишки, машины';
+const productsCategory = 'Игровой приставку, игрушки, мишки, машины';
 const goodsPrice = 5000;
-const selectCategory = productsCategory.slice(54, 61)
+const selectCategory = productsCategory.slice(0, 17)
 
 console.log(typeof goodsCount, typeof goodsPrice)
-console.log(`${productBuy} ${goodsCount} штук. ${selectCategory.toLowerCase()} Общий сумма товара: ${goodsPrice}$`);
+console.log(`${selectCategory} ${productBuy} ${goodsCount} штук.Общий сумма товара: ${goodsPrice}$`);
