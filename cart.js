b0bfd3ed1435a7aff1c0536454b326bbf39beadf
K-Nura <kitayev.n@gmail.com>
2024-06-16 00:17:41 +0500

    IP

--- cart.js
+++ cart.js
@@ -1,29 +1,27 @@
 'use strict';
 
-let cart = {
+const cart = {
   items: [],
   totalPrice: 0,
   count: 0,
-
-  getTotalPrice() {
-    return this.totalPrice
-  },
 
   increaseCount(num) {
     this.count += num
   },
 
   add(name, price, quantity = 1) {
-    let allAdd = { name, price, quantity };
-    this.items.push(allAdd);
+    this.items.push({ name, price, quantity });
     this.increaseCount(quantity);
-    this.calculateItemPrice();
   },
 
   calculateItemPrice() {
-    this.totalPrice = this.items.reduce((acc, item) => {
+    return this.items.reduce((acc, item) => {
       return acc + item.price * item.quantity
     }, 0)
+  },
+
+  get totalPrice() {
+    return this.calculateItemPrice()
   },
 
   clear() {
@@ -33,7 +31,7 @@ let cart = {
   },
 
   print() {
-    console.log(JSON.stringify(this.items));;
+    console.log(JSON.stringify(this.items));
     console.log(`Общый стоимость корзины ${this.totalPrice}`);
   },
 };
@@ -43,3 +41,4 @@ cart.add("Мишка", 500, 2);
 cart.add("Iphone", 1500, 1);
 
 cart.print();
+
